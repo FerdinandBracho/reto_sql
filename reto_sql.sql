1468a2e2582656f4a63f200f4b7ffdac346f8d38
Antonio Servin <antonio.servin.m11@gmail.com>
2021-07-08 00:08:08 -0500

    Correct syntax issues and add script to insert regions

--- reto_sql.sql
+++ reto_sql.sql
@@ -27,7 +27,7 @@ create table if not exists locations(
 -- !Seccion Antonio
 create table if not exists regions(
 	id serial primary key not null,
-	name varchar(10)
+	name varchar(20)
 );
 
 create table if not exists departments(
@@ -36,17 +36,26 @@ create table if not exists departments(
   manager_id int,
   location_id int,
   constraint fk_managers
-		foreign key(manager_id) references managers(id)
+		foreign key(manager_id) references managers(id),
   constraint fk_locations
-    foreign key(location_id) references locations(id)
+  		foreign key(location_id) references locations(id)
 );
 
 create  table if not exists employees(
 	id serial primary key not null,
-  first_name varchar(50),
-  last_name varchar(50),
-  phone_number varchar(20),
-  email varchar(100),
-  hire_date date,
+  	first_name varchar(50),
+  	last_name varchar(50),
+  	phone_number varchar(20),
+	email varchar(100),
+  	hire_date date
 );
+
+
+insert into regions(name)
+values
+	('North America'),
+	('Europe'),
+	('Asia'),
+	('South America'),
+	('Oceania');
 -- !Seccion Ferdinand 
